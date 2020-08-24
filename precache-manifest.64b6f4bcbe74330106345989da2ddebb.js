9c4fbfd4ac08c98e93323decfac06f6861e8490a
andrewroar <59775822+andrewroar@users.noreply.github.com>
2020-08-24 23:04:44 +0100

    Updates

---
+++ precache-manifest.64b6f4bcbe74330106345989da2ddebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ff03ce7e07c01c13cedfdbde236d79",
+    "url": "/react-table/index.html"
+  },
+  {
+    "revision": "ac3b4b6a307cc9e765e6",
+    "url": "/react-table/static/css/main.fb08be5e.chunk.css"
+  },
+  {
+    "revision": "c50f9f9ec2543c503c5e",
+    "url": "/react-table/static/js/2.5ba669a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-table/static/js/2.5ba669a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3b4b6a307cc9e765e6",
+    "url": "/react-table/static/js/main.0bc98ce7.chunk.js"
+  },
+  {
+    "revision": "475b7dea15ce887d6433",
+    "url": "/react-table/static/js/runtime-main.b4d75c4a.js"
+  }
+]);
--- src/components/Sortbutton/index.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-
-const Sortbutton = (props) => {
-  return <button onClick={props.sortByName}>Sort by Name</button>;
-};
-export default Sortbutton;
